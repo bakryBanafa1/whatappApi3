e63d5ee8350c91d8e8fca751256875430670183f
Whatsapp-API1 <banafa00@gmail.com>
2023-07-10 20:19:50 +0300

    initial commit

---
+++ src/controllers/whatsappControllers.js
@@ -0,0 +1,23 @@
+const VerifyToken=(req,res) =>{
+    try{
+     var accessToken ="FLKDGFJKDHJDJ5FG5D2D";
+     var token=req.query["hub.verify_token"];
+     var challenge=req.query["hub.challenge"];
+     if (challenge !=null && token !=null && token == accessToken){
+        res.send(challenge);
+     } else {
+        res.status(400).send();
+     }
+    }
+    catch(e){
+     res.status(400).send;
+    }
+//res.send("hola VerifyToken");
+}
+const receivedMessage =(req,res)=>{
+    res.send("hola Recevied");
+}
+module.exports ={
+    VerifyToken,
+    receivedMessage  
+}
