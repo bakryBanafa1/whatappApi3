de7259cb52733e6d0c1d06c0318a4d7491a56786
bakryBanafa1 <139151027+bakryBanafa1@users.noreply.github.com>
2023-07-10 20:44:23 +0300

    Update whatsappControllers.js

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,17 +1,17 @@
 const VerifyToken=(req,res) =>{
-    try{
-     var accessToken ="FLKDGFJKDHJDJ5FG5D2D";
-     var token= req.query["hub.verify_token"];
-     var challenge= req.query["hub.challenge"];
-     if (challenge !=null && token !=null && token == accessToken){
-        res.send(challenge);
-     } else {
-        res.status(400).send();
-     }
-    }
-    catch(e){
-     res.status(400).send;
-    }
+  //  try{
+   //  var accessToken ="FLKDGFJKDHJDJ5FG5D2D";
+   //  var token= req.query["hub.verify_token"];
+   //  var challenge= req.query["hub.challenge"];
+    // if (challenge !=null && token !=null && token == accessToken){
+    //   res.send(challenge);
+   //  } else {
+   //     res.status(400).send();
+  //   }
+  //  }
+  //  catch(e){
+   //  res.status(400).send;
+ //   }
 res.send("hola world");
 }
 const receivedMessage =(req,res)=>{
