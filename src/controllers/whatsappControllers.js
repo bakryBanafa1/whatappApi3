660f50786ae0a33c04d99f104c07af3ac3e11c35
bakryBanafa1 <139151027+bakryBanafa1@users.noreply.github.com>
2023-07-10 21:57:27 +0300

    Update whatsappControllers.js

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,21 +1,63 @@
+const fs =require("fs")
+const myConsole= new console.Console(fs.createWriteStream("./logs.txt"));
 const VerifyToken=(req,res) =>{
     try{
      var accessToken ="FLKDGFJKDHJDJ5FG5D2D";
-    var token= req.query["hub.verify_token"];
-    var challenge= req.query["hub.challenge"];
+     var token= req.query["hub.verify_token"];
+     var challenge= req.query["hub.challenge"];
      if (challenge !=null && token !=null && token == accessToken){
-      res.send(challenge);
-    } else {
+        res.send(challenge);
+     } else {
         res.status(400).send();
      }
-   }
+    }
     catch(e){
      res.status(400).send;
     }
-res.send("hola world");
+//res.send("hola VerifyToken");
 }
 const receivedMessage =(req,res)=>{
-    res.send("hola Recevied");
+    try{
+        var entry =(req.body["entry"])[0];
+        var changes =(req.body["changes"])[0];
+        var value =changes["value"];
+        var messageObject = value ["messages"]; 
+        var messages = messageObject[0];
+        var text =GetTextUser(messages);
+        myConsole.log(text);
+
+        res.send("EVENT_RECEIVED");
+    }
+    catch(e){
+        myConsole.log(e);
+        res.send("EVENT_RECEIVED");
+    }
+
+   // res.send("hola Recevied");
+   function GetTextUser(messages){
+    var text ="";
+    var typeMessage =messages["type"];
+    if (typeMessage=="text"){
+        text =(messages["text"])["body"];
+
+    }
+    else if (typeMessage =="interactive"){
+        var interactiveObject =messages["interactive"]
+        var typeInteractive =interactiveObject["type"];
+       
+        if (typeInteractive =="button_reply"){
+            text=(interactiveObject["button_reply"])["title"];
+        }
+        else if (typeInteractive=="list_reply"){
+            text=(interactiveObject["list_reply"])["title"];
+        } else {
+            myConsole.log("sin message");
+        }
+    } else {
+        myConsole.log("sin message");
+    }
+     return text;
+   }
 }
 module.exports ={
     VerifyToken,
