1c55a0e1ebf5f2c7ba2ee145852d7a3ccf1dd3ae
bakryBanafa1 <139151027+bakryBanafa1@users.noreply.github.com>
2023-07-10 20:39:09 +0300

    Update whatsappControllers.js

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,8 +1,8 @@
 const VerifyToken=(req,res) =>{
     try{
      var accessToken ="FLKDGFJKDHJDJ5FG5D2D";
-     var token=req.query["hub.verify_token"];
-     var challenge=req.query["hub.challenge"];
+     var token= req.query["hub.verify_token"];
+     var challenge= req.query["hub.challenge"];
      if (challenge !=null && token !=null && token == accessToken){
         res.send(challenge);
      } else {
@@ -12,7 +12,7 @@ const VerifyToken=(req,res) =>{
     catch(e){
      res.status(400).send;
     }
-//res.send("hola VerifyToken");
+res.send("hola VerifyToken");
 }
 const receivedMessage =(req,res)=>{
     res.send("hola Recevied");
